ae0be883e02f6a66eb875ccae2efd5ff86424267
kkedziak <kkedziak@splunk.com>
2025-01-29 14:50:24 +0100

    Change

--- tests/integration/test_rest_handler_handler.py
+++ tests/integration/test_rest_handler_handler.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-from datetime import datetime
 from urllib import parse
 from requests.auth import HTTPBasicAuth
 from splunktaucclib.rest_handler.handler import BASIC_NAME_VALIDATORS
